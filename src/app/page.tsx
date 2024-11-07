9ed04e18ccbfb62eb50287ce6ee868ab5c61dcd9
SughraRasheed <159168716+SughraRasheed@users.noreply.github.com>
2024-11-08 02:23:28 +0500

    fixed more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,16 @@ export default function Home() {
     <div className="md:max-w-screen-md lg:max-w-screen-lg "> 
       <h1 className="mx-28 my-11 text-6xl text-green-800 lg:w-[500px]">Transform Your Space with Green</h1>
       <p className="mx-28 text-xl lg:w-[500px]">
-
 "Discover the beauty and benefits of indoor plants with our curated collection. Transform your space with lush greenery and expert care tips for every plant lover."
       </p>
+     
+        <button className="bg-green-800 text-white text-2xl px-6 py-2 rounded-lg hover:bg-green-700  mx-28 my-11">Explore More</button>
+      
     </div>
     <div className=" bg-[url('/images/plant.png')] w-400px] h-[400px] lg:w-[450px] lg:h-[600px] bg-cover mx-auto my-auto "></div>
     </div>
+      
+
       </section>
     </main>
   );
